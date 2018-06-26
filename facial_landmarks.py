d48814a181d5c76c3ce7063655dddd5f99457655
Jiashuo Zhang <jiashuoz@outlook.com>
2018-06-26 14:49:47 +0800

    create face dataset

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -19,52 +19,73 @@ import cv2
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("./predictor/shape_predictor_68_face_landmarks.dat")
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+# http://www.paulvangent.com/2016/08/05/emotion-recognition-using-facial-landmarks/
+# get all landmarks in a list in the format x1, y1, x2, y2, etc.
+def get_landmarks(image):
+    detections = detector(image, 1)
+    landmarks = []
+    for k, d in enumerate(detections):  # For all detected face instances individually
+        shape = predictor(image, d)  # Draw Facial Landmarks with the predictor class
+        xlist = []
+        ylist = []
+        for i in range(1, 68):  # Store X and Y coordinates in two lists
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
 
-    # convert the captured image to grayscale
-    frame = imutils.resize(frame, width=400)
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
-
-        # convert dlib's rectangle to a OpenCV-style bounding box
-        # [i.e., (x, y, w, h)], then draw the face bounding box
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
-
-        # show the face number
-        cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        for (x, y) in shape:
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-
-    # show the output image with the face detections + facial landmarks
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        for x, y in zip(xlist, ylist):  # Store all landmarks in one list in the format x1,y1,x2,y2,etc.
+            landmarks.append(x)
+            landmarks.append(y)
+    if len(detections) > 0:
+        return landmarks
+    else:  # If no faces are detected, return error message to other function to handle
+        landmarks = "error"
+        return landmarks
 
 
+def run():
+    cap = cv2.VideoCapture(0)
 
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
+        # convert the captured image to grayscale
+        frame = imutils.resize(frame, width=400)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces in the grayscale image
+        rects = detector(gray, 1)
+
+        # loop over the face detections
+        for (i, rect) in enumerate(rects):
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            # convert dlib's rectangle to a OpenCV-style bounding box
+            # [i.e., (x, y, w, h)], then draw the face bounding box
+            (x, y, w, h) = face_utils.rect_to_bb(rect)
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+            # show the face number
+            cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+            # loop over the (x, y)-coordinates for the facial landmarks
+            # and draw them on the image
+            for (x, y) in shape:
+                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+        # show the output image with the face detections + facial landmarks
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
